b17c76930278d8b95259976ff660c38c56f7f78d
Rodrigo <76541083+C-RodrigoBS@users.noreply.github.com>
2023-08-20 22:38:37 -0500

    v1

--- Paseo Central/js/click-scroll.js
+++ Paseo Central/js/click-scroll.js
@@ -1,37 +1,41 @@
-//jquery-click-scroll
-//by syamsul'isul' Arifin
-
 var sectionArray = [1, 2, 3, 4, 5, 6];
 
-$.each(sectionArray, function(index, value){
-          
-     $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 83;
-         var docScroll = $(document).scrollTop();
-         var docScroll1 = docScroll + 1;
-         
-        
-         if ( docScroll1 >= offsetSection ){
-             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-         }
-         
-     });
-    
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 83;
+$.each(sectionArray, function (index, value) {
+    $('.click-scroll').eq(index).click(function (e) {
         e.preventDefault();
+        var offsetClick = $('#' + 'section_' + value).offset().top;
+        if (index === 0) {
+            offsetClick -= 83; // Restaurar el padding para el primer section
+        }
         $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
+            'scrollTop': offsetClick
+        }, 300);
     });
-    
 });
 
-$(document).ready(function(){
-    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
+$(document).scroll(function () {
+    var docScroll = $(document).scrollTop();
+
+    $.each(sectionArray, function (index, value) {
+        var sectionSelector = '#section_' + value;
+        var offsetSection = $(sectionSelector).offset().top;
+        var sectionHeight = $(sectionSelector).height();
+
+        if (index > 0) {
+            offsetSection -= 83; // Ignorar el padding para los siguientes sections
+        }
+
+        if (docScroll >= offsetSection && docScroll < offsetSection + sectionHeight) {
+            $('.navbar-nav .nav-item .nav-link').removeClass('active');
+            $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
+            $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+            $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
     $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
     $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
 });
